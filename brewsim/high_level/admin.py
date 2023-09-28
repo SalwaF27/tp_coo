8a84df779739bd9cd91309fa0b2e305e81580bba
Salwa Charai <chs2737a@u1-201-06d.univ-tlse3.fr>
2023-09-28 15:12:33 +0200

    add high_level models & admin

--- brewsim/high_level/admin.py
+++ brewsim/high_level/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import Departement, Machine, Ingredient, QuantiteIngredient, Action, Recette, Usine, Prix
+
+admin.site.register(Departement)
+admin.site.register(Machine)
+admin.site.register(Ingredient)
+admin.site.register(QuantiteIngredient)
+admin.site.register(Action)
+admin.site.register(Recette)
+admin.site.register(Usine)
+admin.site.register(Prix)
--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -4,17 +4,25 @@ from django.db import models
 class Departement(models.Model):
         numero = models.IntegerField()
         prixparMcarre = models.DecimalField(max_digits=10, decimal_places=2)
+        def __str__(self):
+                return f"{self.numero} {self.prixparMcarre}"
 
 class Machine(models.Model):
         nom = models.CharField(max_length=100)
         prix = models.IntegerField()
+        def __str__(self):
+                return f"{self.nom} {self.prix}"
 
 class Ingredient(models.Model):
         nom = models.CharField(max_length=100)
+        def __str__(self):
+                return f"{self.nom}"
 
 class QuantiteIngredient(models.Model):
         ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
         quantite = models.IntegerField()
+        def __str__(self):
+                return f"{self.ingredient} {self.quantite}"
 
 class Action(models.Model):
         machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
@@ -22,10 +30,14 @@ class Action(models.Model):
         duree = models.IntegerField()
         ingredients = models.ManyToManyField(QuantiteIngredient)
         action = models.CharField(max_length=100)
+        def __str__(self):
+                return f"{self.machine} {self.commande} {self.duree} {self.ingredient} {self.action}"
 
 class Recette(models.Model):
         nom = models.CharField(max_length=100)
         action = models.ForeignKey(Action, on_delete=models.PROTECT)
+        def __str__(self):
+                return f"{self.nom} {self.action}"
 
 class Usine(models.Model):
         departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
@@ -33,8 +45,12 @@ class Usine(models.Model):
         machines = models.ManyToManyField(Machine)
         recettes = models.ManyToManyField(Recette)
         stocks = models.ManyToManyField(QuantiteIngredient)
+        def __str__(self):
+                return f"{self.departement} {self.taille} {self.machines} {self.recettes} {self.stocks}"
 
 class Prix(models.Model):
         ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
         departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
         prix = models.IntegerField()
+        def __str__(self):
+                return f"{self.ingredient} {self.departement} {self.prix}"
